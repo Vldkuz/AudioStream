196e7e4f2d02d5f6a1a4ac6a02ecf2dc2142512e
Kirill Kostogryzov <dubonosikk@gmail.com>
2024-11-21 01:08:15 +0500

    Начал писать тесты на TrackRepository

--- src/kt/test/nonMock/InfraTests.kt
+++ src/kt/test/nonMock/InfraTests.kt
@@ -2,31 +2,32 @@ package kt.test.nonMock
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.util.*
 import kotlinx.coroutines.runBlocking
-import kt.main.core.Auth
-import kt.main.core.UProfile
-import kt.main.core.User
-import kt.main.infra.AuthTable
-import kt.main.infra.UserProfilesTable
-import kt.main.infra.UsersTable
+import kt.main.core.*
+import kt.main.infra.*
+import kt.main.infra.repositories.TrackRepository
 import kt.main.infra.repositories.UserRepository
+import kt.test.fakers.FakeTrackGenerator
 import kt.test.fakers.FakeUserGenerator
+import kt.webDav.YandexFileManager
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.testng.AssertJUnit.assertEquals
 import org.testng.annotations.BeforeClass
+import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
 import java.util.*
 
 
 open class BaseInfraTest {
-    private val user = System.getenv("TEST_USER")
-    private val password = System.getenv("TEST_PASSWORD")
+    private val dbUser = System.getenv("TEST_USER")
+    private val dbPassword = System.getenv("TEST_PASSWORD")
     private val dbUrl = System.getenv("TEST_DB_URL")
 
     protected val dbInstance = Database.connect(
-        dbUrl, user = user, password = password,
+        dbUrl, user = dbUser, password = dbPassword,
     )
 
     private val httpClient = HttpClient(CIO)
@@ -138,3 +139,119 @@ class UserRepositoryTest : BaseInfraTest() {
 }
 
 //TODO("Надо тесты еще дописать на TrackRepository, RoomRepository")
+class TrackRepositoryTest : BaseInfraTest() {
+    private val webdavPassword = System.getenv("TEST_WEBDAV_PASSWORD")
+    private val webdavLogin = System.getenv("TEST_WEBDAV_LOGIN")
+    private val token = "Basic " + "$webdavLogin:$webdavPassword".encodeBase64()
+
+    private var userRepository: UserRepository? = null
+    private var trackRepository: TrackRepository? = null
+
+    private val testDataCounter = 5
+    private val testData = testTracksAndUsers(testDataCounter)
+    private val testTracks = testData.first
+    private val testUsers = testData.second
+
+
+    private fun testTracksAndUsers(n: Int): Pair<List<Track>, List<User>> {
+        val faker = FakeTrackGenerator()
+        val users = LinkedList<User>()
+        val tracks = LinkedList<Track>()
+
+        for (i in 0..n) {
+            val fakeTrack = faker.createInstance()
+            val fakeUser = fakeTrack.tProfile.uploader
+
+            tracks.add(fakeTrack)
+            users.add(fakeUser)
+        }
+
+        return Pair(tracks, users)
+    }
+
+    @BeforeTest
+    fun setup() {
+        transaction(dbInstance) {
+            SchemaUtils.drop(TrackTable, TrackProfilesTable)
+            SchemaUtils.drop(UsersTable, UserProfilesTable, AuthTable)
+            userRepository = UserRepository(dbInstance)
+            trackRepository = TrackRepository(
+                dbInstance,
+                YandexFileManager(token, HttpClient(CIO)),
+                userRepository!!
+            )
+        }
+
+        runBlocking {
+            for (user in testUsers) {
+                userRepository!!.add(user)
+            }
+        }
+
+        runBlocking {
+            for (track in testTracks) {
+                trackRepository!!.add(track)
+            }
+        }
+    }
+
+    @Test
+    fun testGetAll() {
+        val allTracks = runBlocking {
+            trackRepository!!.getAll()
+        }
+
+        assertEquals(allTracks, testTracks)
+    }
+
+    @Test
+    fun testGetByName() {
+        val trackName = testTracks[0].tProfile.name
+        val tracksWithName = testTracks.filter { it.tProfile.name == trackName }
+
+        val tracksInDB =  runBlocking {
+            trackRepository!!.getByName(trackName)
+        }
+
+        assertEquals(tracksWithName, tracksInDB)
+    }
+
+    @Test
+    fun testGetById() {
+        val track = testTracks[0]
+        val id = track.id
+
+        val trackInDb = runBlocking {
+            trackRepository!!.getById(id)
+        }
+
+        assertEquals(track, trackInDb)
+    }
+
+    /*@Test
+    fun testUpdate() {
+        val id = testTracks[0]
+
+        val rProfile = TProfile(
+            "Jostky Move",
+            "Slavik_1994",
+        )
+
+        val auth = Auth(
+            "loginov",
+            "Biba023543"
+        )
+
+        val user = User(rProfile, auth, id)
+
+        runBlocking {
+            userRepository!!.update(user)
+        }
+
+        val userInDb = runBlocking {
+            userRepository!!.getById(id)
+        }
+
+        assertEquals(user, userInDb)
+    }*/
+}
